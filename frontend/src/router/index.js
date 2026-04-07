837b19ffeecf7074b7d75b501a4001af7f02b634
Zebq <74563976+Zebq@users.noreply.github.com>
2026-04-07 14:20:19 +0200

    Update index.js
    cambios en index.js para visualizar el login

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,4 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
+import createApp from 'vue'
+import App from './App.vue'
+import router from './router'
 import LoginView from '../componentes/LoginView.vue'
 import Incidencias from '../views/Incidencias.vue'
 import Comunidades from '../views/Comunidades.vue'
@@ -13,5 +16,9 @@ const router = createRouter({
   history: createWebHistory(),
   routes
 })
+
+const app= createApp(App)
+app.use(router)
+app.mount('#app')
 
 export default router

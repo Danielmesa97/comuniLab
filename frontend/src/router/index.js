d819be4951fff416e2c753fcd3034b86b4c3f4ac
Zebq <74563976+Zebq@users.noreply.github.com>
2026-04-07 14:04:46 +0200

    Update index.js
    añadir vista al Vue

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,8 +1,10 @@
 import { createRouter, createWebHistory } from 'vue-router'
+import LoginView from '../componentes/LoginView.vue'
 import Incidencias from '../views/Incidencias.vue'
 import Comunidades from '../views/Comunidades.vue'
 
 const routes = [
+  {path: '/', name: 'login', component: LoginView},
   { path: '/', component: Incidencias },
   { path: '/comunidades', component: Comunidades }
 ]

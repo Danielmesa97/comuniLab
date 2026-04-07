a14ec287916a2d602f23a132f1ef43d9ee7433ce
Zebq <74563976+Zebq@users.noreply.github.com>
2026-04-07 14:16:25 +0200

    Update App.vue
    cambios en APP.vue para mostrar el loginView

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,10 +1,16 @@
 <template>
-  <div>
-    <nav>
-      <a href="/">Incidencias</a> |
-      <a href="/comunidades">Comunidades</a>
+  <div id="app-container">
+    <nav v-if="$route.path !== '/'">
+      <router-link to="/">LoginView</router-link> |
+      <router-link to="/">Incidencias</router-link> |
+      <router-link to ="/comunidades">Comunidades</router-link>
     </nav>
 
     <router-view />
   </div>
 </template>
+
+<script setup>
+  import {useRoute} from 'vue-router'
+  const route= useRoute()
+</script>

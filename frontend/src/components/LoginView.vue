52d2d34389702b25a53abaaa62628c54b08da8a9
Zebq <74563976+Zebq@users.noreply.github.com>
2026-04-07 13:38:22 +0200

    Create LoginView.vue

---
+++ frontend/src/components/LoginView.vue
@@ -0,0 +1,21 @@
+//Inicio de Sesion
+<script setup>
+import {ref} from Vue
+const isLogin=ref(true)</script>
+<template>
+    <div class="main-container">
+        <div class="form-card">
+            <div class="logo container">
+                <div class="logo-circle">
+                    <span class="logo-placeholder">LOGO</span>
+                </div>
+            </div>
+        </div>
+        //Iniciar Sesion
+        <div class="tab-selector">
+            <button @click="isLogin=true"
+            :class="['tab-button',isLogin ? 'active':'inactive']">
+        </button>
+        </div>
+    </div>
+</template>

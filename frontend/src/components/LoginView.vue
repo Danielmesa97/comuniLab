284883f91fce3c93fe0d03aa511966cfd1634dcf
Zebq <74563976+Zebq@users.noreply.github.com>
2026-04-07 13:52:34 +0200

    Update LoginView.vue

--- frontend/src/components/LoginView.vue
+++ frontend/src/components/LoginView.vue
@@ -19,3 +19,33 @@ const isLogin=ref(true)</script>
         </div>
     </div>
 </template>
+
+<style scoped>
+    .main-container{
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        background-color: #333; /* color gris parecido al wireframe*/
+    }
+    .form-card{
+        width: 300 px;
+        min-height: 500 px;
+        border-radius: 30 px;
+        padding: 40 px 30 px;
+        display: flex;
+        flex-direction: column;
+    }
+    .tab-selector{
+        display: flex;
+        border-radius: 10 px;
+        padding: 5 px;
+        margin-bottom: 20 px;
+        background-color: #f2f2f2;
+    }
+    .tab-button{
+        flex: 1;
+        border: none;
+        font-size: 12 px;
+        padding: 10 px;
+    }
+</style>

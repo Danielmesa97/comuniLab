644f907cf17f89700de40565268451afd2286657
Zebq <74563976+Zebq@users.noreply.github.com>
2026-04-09 14:24:46 +0200

    Update index.js

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -2,10 +2,12 @@ import { createRouter, createWebHistory } from 'vue-router'
 import LoginView from '../components/LoginView.vue'
 import Incidencias from '../views/Incidencias.vue'
 import Comunidades from '../views/Comunidades.vue'
+import DashboardView from '../views/DashboardView.vue'
 
 const routes = [
   {path: '/', name: 'login', component: LoginView},
-  { path: '/', component: Incidencias },
+  {path:'/dashboard', name:'dashboard', component: DashboardView },
+  { path: '/incidencias', component: Incidencias },
   { path: '/comunidades', component: Comunidades }
 ]
 

640378e18e4bbf3bb7f0be82e120c80d408edafe
PereAPrats <ppratsv03@gmail.com>
2026-05-06 17:49:28 +0200

    Comentar la inclusión del middleware EnsureFrontendRequestsAreStateful en la configuración de la aplicación.

--- backend/bootstrap/app.php
+++ backend/bootstrap/app.php
@@ -4,7 +4,7 @@ use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 use Illuminate\Auth\AuthenticationException;
-use Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful;
+//use Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -21,10 +21,10 @@ return Application::configure(basePath: dirname(__DIR__))
         });
     })
     ->withMiddleware(function (Middleware $middleware): void {
-        // 🔥 AÑADE ESTO (MUY IMPORTANTE)
-        $middleware->append(EnsureFrontendRequestsAreStateful::class);
-
-        // ya lo tenías
+//        // 🔥 AÑADE ESTO (MUY IMPORTANTE)
+//        $middleware->append(EnsureFrontendRequestsAreStateful::class);
+//
+//        // ya lo tenías
         $middleware->append(\Illuminate\Http\Middleware\HandleCors::class);
     })
     ->create();
